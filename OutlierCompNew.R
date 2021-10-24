47102514aadda3b22526ad7846025842e1cfaf85
Paul Harmon <paulharmongj@gmail.com>
2021-10-23 21:31:47 -0600

    adding a bunch of files to repo

---
+++ OutlierCompNew.R
@@ -0,0 +1,156 @@
+## Draft version of outlier comparison function agnostic to mapping technique
+
+
+# preliminary functions: 
+
+## Vectorize Dist: Function that takes a dataframe and returns a vectorized version of a distance matrix applied to that data matrix. 
+## This function is used for generating vectorized data when we step through the Mantel test. 
+
+vectorize_dist <- function(df) {
+  tmp <- dist(df)
+  vec <- c(tmp)
+  return(vec)}
+
+## Pull P-Val and Pull F stat return the p-values and f-statstics from the list of output MultiPermanova puts out. 
+
+pullPval <- function(x){temp <- x$aov.tab$`Pr(>F)`[1]; return(temp)}
+
+pullFstat <- function(x){temp <- x$aov.tab$`F.Model`[1]; return(temp)}
+
+
+
+
+
+
+#generate maps on holdout data (jth iteration is held out)
+# this function should be able to operate on multiple mapping techniques or a single one 
+
+Generate_Holdout_Maps <- function(data, j, maptype = "MDS", perp_val = 10){  
+  
+  #hold out a single row of the data (in the jth row)
+  #note - have to remove, as t-SNE doesn't like NAs
+  data_holdout <- data[-j,]
+  
+  #calculate distances FIRST
+  data_dist <- dist(data_holdout)
+  
+  #### Creates Maps (based on several methods: tSNE and MDS currently)
+  
+  if(maptype %in% c("MDS", "mds")){
+    #generate the classical MDS in 2 dimensions
+    map <- cmdscale(data_dist, k = 2)
+  }
+  else if(maptype == "tSNE"){
+    #generate the t-SNE map and store x,y in map list
+    map <- Rtsne(data_dist, perplexity = perp_val, pca = pca_options, is_distance = TRUE)$Y
+  }
+  else{print("No maptype selected.")}
+  
+  
+  return(list(map = map))
+}
+
+
+
+
+#mapping method supported
+
+MultiPermanova <- function(data, nperms = 1000, perp_val= 10, pca_options = FALSE, maptype = "MDS"){
+  
+  # initialize the size of the data and output lists
+  n <- nrow(data)
+  maplist <- list()
+  
+  
+  #generate maps on holdout data
+  for(j in 1:n){
+    
+    temp <- Generate_Holdout_Maps(data, j, maptype = maptype)
+    maplist[[j]] <- temp$map
+    
+  }
+  
+  
+  # Insert NAs into the holdout indices 
+  maplist_new <- list()
+  
+  #
+  for(j in 1:length(maplist)){
+    #create temporary vectors with NAs for each of the XY coordingates
+    tempX <- R.utils::insert(maplist[[j]][,1], j, NA)
+    tempY <- R.utils::insert(maplist[[j]][,2], j, NA)
+    
+    
+    maplist_new[[j]] <- tibble(X = tempX, Y = tempY)
+    
+  }
+  
+  #vectorizes the distances and outputs a list of vectorized distance matrices 
+  dist_list <- lapply(maplist_new, vectorize_dist)
+  
+  
+  ## takes the vectorized distance matrices and smashes them into a single data frame - to build correlation matrix
+  
+  dist_mat <- dist_list %>% as_tibble(.name_repair = "minimal")
+  
+  #had to remove the NAs here to get this to run (as it removes when I try to use complete.obs later on)
+  #dist_df_na <- sapply(dist_mat, na.omit) %>% as_tibble(.name_repair = "minimal")
+  #since I'm using pairwise complete obs, I shouldn't need to do this
+  
+  cormat <- cor(dist_mat, use = "pairwise.complete.obs", method = "pearson")
+  cormat %>% dim()
+  
+  
+  ##generate distance on correlation matrix
+  cordist <- sqrt(2*(1-cormat)) %>% as.dist()
+
+  model_list <- list()
+
+  
+  #Step through the permanova method: 
+  
+  for(j in 1: nrow(as.matrix(cordist))){
+    #generate the indicator variable for each feature
+    indicator_df <- rep(0, nrow(maplist_new[[1]])) %>% as.data.frame()
+    
+    
+    names(indicator_df) <- "Indicator"
+    indicator_df$Indicator[j] = 1
+    
+    ### create the permutation matrix
+    #initialize
+    permat1 <- matrix(0, nrow = length(indicator_df$Indicator), ncol = length(indicator_df$Indicator))
+    permat1[1,] <- 1:length(indicator_df$Indicator)
+    
+    for(i in 2:nrow(permat1)){
+      permat1[i,] <- c(i:length(indicator_df$Indicator), 1:(i-1))
+    }
+  
+    
+    #each item in the list is an Adonis test using the indicator for the holdout variable
+    model_list[[j]] <- adonis(cordist ~ Indicator, data = indicator_df, permutations = permat1, parallel = 2)
+    
+    #returns the list of ADONIS outputs
+    
+    
+  }
+  
+  return(list(modellist = model_list, plot_map = maplist))
+}
+
+
+#quick testing
+#  tic = Sys.time()
+#  x3 = MultiPermanova(yes_structure_final[,1:10], maptype = "MDS")
+#  toc = Sys.time()
+# # 
+#  toc - tic
+# # 
+#  tic = Sys.time()
+#  x4 = MultiPermanova(yes_structure_final[,1:10], maptype = "tSNE", perp_val = 20)
+#  toc = Sys.time()
+#  
+#  toc - tic
+
+#both functions take about 2.5 minutes
+
---
+++ SimulationCodeScript.R
@@ -0,0 +1,192 @@
+## Simulation Script
+## October 5, 2021
+
+## Paul Harmon
+
+#Description: This script simulates data and runs the different mapping techniques over a span of several different runs. 
+# We will compare several scenarios. 
+
+
+
+#### Preliminary code -----------------------------------------------------------#
+
+# Library and source relevant functions
+source("OutlierCompNew.R")
+library(ggplot2)
+library(readr)
+library(tibble)
+library(dplyr)
+library(tidyr)
+library(janitor)
+library(magrittr)
+library(knitr)
+library(Rtsne)
+library(robustHD)
+library(GGally)
+library(stringr)
+library(plotly)
+library(R.utils)
+library(vegan)
+library(mnormt)
+
+
+
+#### Initialize lists and variables ---------------------------------------------#
+
+n_sims = 100
+
+#create a bunch of lists to store stuff later
+plot_data_list <- list()
+plot_tsne_results_list <- list()
+plot_mds_results_list <- list()
+plot_tsne_fstat_results_list <- list()
+plot_mds_fstat_results_list <- list()
+results_list <- list()
+results_list_cmd <- list()
+df3list = df2list = list()
+perf_time <- list()
+perf_time_cmd <- list()
+
+
+#### Simulate the Data and Loop 
+
+for(j in 1:n_sims){
+  
+  #### Simulate the Data with outliers and a 3 groups (simulates all from one group with mean 40)
+  
+  g1 <- rmnorm(n = 10, mean = rep(20, 20), diag(35, 20)) %>%
+    t() %>%
+    data.frame()
+  g2 <- rmnorm(n = 10, mean = rep(40, 20), diag(35, 20)) %>%
+    t() %>%
+    data.frame()
+  g3 <- rmnorm(n = 10, mean = rep(60, 20), diag(35, 20)) %>%
+    t() %>%
+    data.frame()
+  
+  
+  yes_structure <- rbind(g1, g2, g3) %>%
+    data.frame() %>%
+    mutate(Group = c(rep("1", 20), rep("2", 20), rep("3", 20)))
+  
+  yes_structure_scale <- lapply(yes_structure[,1:10], scale) %>% as_tibble()
+  
+  ### adds outliers - but not as a single group. Here we have an outlier that includes a really big one, a really small observation, and a very abnormal one
+  #add_outliers <- rbind(rnorm(10, 3, .5), rnorm(10, -3, .5), rnorm(10, c(rep(-2,3),rep(0,3),rep(2,3),0), .5)) %>% as_tibble()
+  
+  add_outliers <- rbind(rnorm(10, 0, 1)) %>% as_tibble()
+  
+  names(add_outliers) <- names(yes_structure_scale)
+  
+  yes_structure_final <- bind_rows(yes_structure_scale, add_outliers) 
+  yes_structure_final$Group <- c(yes_structure$Group, rep("Outlier",1))
+  
+  
+  ##### Visualize the data and store in a list
+  datplot <- yes_structure_final %>% mutate(Id = 1:nrow(yes_structure_final), Alpha = ifelse(Group %in% 'Outlier', 1,0.8)) %>%  pivot_longer(1:10) %>% ggplot(aes(name, value, color = Group, group = Id, alpha = Alpha)) + geom_line() + geom_point() + ggtitle("Simulated Data with Outliers Added In") + guides(alpha = FALSE)
+  plot_data_list[[j]] <- datplot
+  
+  
+  
+  #### Run the Permanova mapping (for t-SNE)
+  tic = Sys.time()
+  xTSNE <- MultiPermanova(yes_structure_final[,1:10], maptype = "tSNE", perp_val = 20)
+  toc = Sys.time() 
+  perf_time[[j]] <- toc - tic
+  
+  
+  results_list[[j]] <- xTSNE
+  
+  df2 = tibble(Holdout = 1:length(xTSNE$modellist), PValues = sapply(xTSNE$modellist, pullPval), Fstat = sapply(xTSNE$modellist, pullFstat))
+  df2$Colors <- ifelse(df2$PValues < 0.05, TRUE, FALSE)
+  df2list[[j]] = df2
+  
+  #### TSNE Plots########################################################
+  #Visualize the results and store in a list
+  sigplot <- ggplot(df2, aes(Holdout, PValues)) + geom_point(aes(color = Colors), size = 2) + geom_line(alpha = 0.5, color = "grey") + theme_bw() + ggtitle("t-SNE: P-Values From Adonis")
+  plot_tsne_results_list[[j]] <- sigplot
+  
+  #Visualize the f stats and store in a list
+  sigplotf <- ggplot(df2, aes(Holdout, Fstat)) + geom_point(aes(color = Colors), size = 2) + geom_line(alpha = 0.5, color = "grey") + theme_bw() + ggtitle("t-SNE: F Stats From Adonis")
+  plot_tsne_fstat_results_list[[j]] <- sigplotf
+  
+  
+  
+  #### Run the Permanova mapping (for MDS)
+  tic = Sys.time()
+  xCMD <- MultiPermanova(yes_structure_final[,1:10], maptype = "mds")
+  toc = Sys.time()
+  results_list_cmd[[j]] <- xCMD
+  perf_time_cmd[[j]] <- toc - tic
+  
+  ### CMD Plots #########################################################
+  
+  df3 = tibble(Holdout = 1:length(xCMD$modellist), PValues = sapply(xCMD$modellist, pullPval), Fstat = sapply(xCMD$modellist, pullFstat))
+  df3$Colors <- ifelse(df3$PValues < 0.05, TRUE, FALSE)
+  df3list[[j]] = df3
+  
+  
+  #Visualize the results and store in a list
+  sigplot2 <- ggplot(df3, aes(Holdout, PValues)) + geom_point(aes(color = Colors), size = 2) + geom_line(alpha = 0.5, color = "grey") + theme_bw() + ggtitle("CMD: P-Values From Adonis")
+  plot_mds_results_list[[j]] <- sigplot2
+  
+  #Visualize the f stats and store in a list
+  sigplot2f <- ggplot(df3, aes(Holdout, Fstat)) + geom_point(aes(color = Colors), size = 2) + geom_line(alpha = 0.5, color = "grey") + theme_bw() + ggtitle("CMD: F Stats From Adonis")
+  plot_mds_fstat_results_list[[j]] <- sigplot2f
+  
+  
+}
+
+
+#### Some methods to get to table of values to assess efficacy of detections -----#
+
+# Correctly Identified Influential
+#tmp = tail(df3list[[1]], 3)
+
+correctInfluence = function(result_df){
+  tmp <- tail(result_df,3)
+  det_rate <- length(which(tmp$PValues <= 0.05))/nrow(tmp)
+  return(det_rate)}
+
+# Correctly Identified Non-Influential
+#head(df3list[[1]], 60)
+
+correctNonInfluence = function(result_df){
+  tmp <- head(result_df,60)
+  det_rate <- length(which(tmp$PValues > 0.05))/nrow(tmp)
+  return(det_rate)}
+
+
+#### Generate a table of values to assess efficacy of detections -----------------#
+
+#note - these will give values per run - so we need to take a look across the simulations
+
+#t-SNE
+ci1 <- sapply(df2list, correctInfluence)
+cni1 <- sapply(df2list, correctNonInfluence)
+
+
+#MDS
+ci2 <- sapply(df3list, correctInfluence)
+cni2 <- sapply(df3list, correctNonInfluence)
+
+
+#mean detection rates
+mean(ci1)
+mean(cni1)
+
+mean(ci2)
+mean(cni2)
+
+## Code runs in about 2.5 hours with 100 simulations
+
+## Which components do we save? 
+##
+
+#saves the output tables with p-values and F-stats (we can make plots from this)
+saveRDS(df2list, paste0(Sys.Date(), "_simulation1_tsne_list.RDS"))
+saveRDS(df3list, paste0(Sys.Date(), "_simulation1_mds_list.RDS"))
+
+
+
+
